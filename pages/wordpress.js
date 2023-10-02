0fc2cdb2d02ad1b5173a8a94dbe49aa94e9697dc
Emiliano Tosar <emiliano.r.tosar@gmail.com>
2023-10-02 11:41:48 -0300

    add text

--- pages/wordpress.js
+++ pages/wordpress.js
@@ -12,7 +12,8 @@ const Proyects = () => {
           Proyectos hechos en Wordpress
         </Heading>
         <Text mt={4} mb={8} align={'justify'} color={useColorModeValue('black', 'gray')}>
-          Desde 2022 me propusieron trabajar en Wordpress. Como siempre me interesó más el código que el diseño en sí, aproveché esta oportunidad para interiorizarme en el lenguaje sobre el que está construido WP y en la arquitectura del mismo. Actualmente me encuentro aprendiendo PHP, y estoy estudiando sobre desarrollo de themes, plugins, custom fields, custom post types, la api de Wordpress, etc., para agregarle valor al diseño en Wordpress y que no sea un mero copiar y pegar bloques.
+          Desde 2022 me encuentro trabajando como desarrollador Wordpress. Como siempre me interesó más el código que el diseño en sí, aproveché esta oportunidad para interiorizarme en el lenguaje sobre el que está construido WP y en la arquitectura del mismo. Actualmente me encuentro aprendiendo PHP, y estoy estudiando sobre desarrollo de themes, plugins, custom fields, custom post types, la api de Wordpress, etc., para agregarle valor al diseño en Wordpress y que no sea un mero copiar y pegar bloques.
+          Llevo creados más de 15 sitios, a continuación te dejo una pequeña muestra de mis trabajos:
         </Text>
         <SimpleGrid columns={[1, 1, 3]} spacing={8} minChildWidth="200px">
           {wordpressWorks.map((proyecto, i) => {
