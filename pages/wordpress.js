966fdb1a5971f99b7d9fbad2a84f2a0fcabb01e2
Emiliano Tosar <emiliano.r.tosar@gmail.com>
2024-07-31 14:04:07 -0300

    change in texts

--- pages/wordpress.js
+++ pages/wordpress.js
@@ -33,11 +33,15 @@ const Proyects = () => {
           align={'justify'}
           color={useColorModeValue('black', 'gray')}
         >
-          Tengo experiencia en el desarrollo de themes, plugins, manejo de
-          custom fields, custom post types, uso de la api de Wordpress, bloques
-          Gutenberg. También trabajo haciendo limpieza de malware, levantamiento
-          de sitios caídos, mejoras de seguridad, y todo lo que sea
-          mantenimiento web.
+          Tengo experiencia en el desarrollo y personalización de temas de
+          WordPress, incluyendo la creación de child themes. Soy experto en la
+          configuración y optimización de plugins, así como en el desarrollo de
+          soluciones personalizadas. Manejo prácticas de seguridad, gestión de
+          usuarios y optimización SEO. Además, tengo conocimientos en HTML, CSS,
+          y JavaScript para personalizaciones avanzadas, y experiencia en
+          e-commerce con WooCommerce. También realizo migraciones de sitios y
+          utilizo herramientas como Google Analytics para mejorar el rendimiento
+          de los sitios.
         </Text>
         <Text
           mt={4}
@@ -45,7 +49,7 @@ const Proyects = () => {
           align={'justify'}
           color={useColorModeValue('black', 'gray')}
         >
-          A continuación te dejo una pequeña muestra de mis trabajos:
+          A continuación, una pequeña muestra de mis trabajos:
         </Text>
         <SimpleGrid columns={[1, 1, 2]} spacing={8} minChildWidth="300px">
           {wordpressWorks.map((proyecto, i) => {
